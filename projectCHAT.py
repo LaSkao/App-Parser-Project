6b2c89f284a76697d4ff75db168b7dfab105d58c
LaSkao <106691937+LaSkao@users.noreply.github.com>
2023-03-28 13:15:30 +0300

    Update projectCHAT.py

--- projectCHAT.py
+++ projectCHAT.py
@@ -3,7 +3,7 @@ import json
 from bs4 import BeautifulSoup
 
 # url страницы, которую мы хотим спарсить
-url = "https://example.com/products"
+url = "https://online.metro-cc.ru/category/alkogolnaya-produkciya/krepkiy-alkogol"
 
 # отправляем GET-запрос на получение HTML-кода страницы
 response = requests.get(url)
@@ -12,24 +12,24 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
 # находим все div-контейнеры с информацией о продуктах на первой странице
-products = soup.findall('div', class='product-info')
+products = soup.findall('div', class_='catalog-2-level-product-card product-card subcategory-or-type__products-item catalog--online offline-prices-sorting--all with-prices-drop')
 
 # для каждого продукта получаем название и цену
 result = []
 for product in products:
-    name = product.find('h2').text.strip()
-    price = product.find('div', class_='price').text.strip()
+    name = product.find('span', class_='product-card-name__text').text.strip()
+    price = product.find('span', class_='product-price__sum-rubles').text.strip()
     result.append({"name": name, "price": price})
 
 # проверяем, есть ли на странице ссылка на следующую страницу
-nextpage = soup.find('a', class='next-page')
-while next_page:
+nextpage = soup.find('a', class_='v-pagination__navigation catalog-paginate__item')
+while nextpage():
 
     # получаем ссылку на следующую страницу и повторяем все те же шаги
-    url = next_page['href']
+    url = nextpage['href']
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    products = soup.findall('div', class='product-info')
+    products = soup.findall('div', class_='product-info')
 
     for product in products:
         name = product.find('h2').text.strip()
@@ -37,60 +37,8 @@ while next_page:
         result.append({"name": name, "price": price})
 
     # ищем ссылку на следующую страницу на текущей странице
-    nextpage = soup.find('a', class='next-page')
+    nextpage = soup.find('a', class_='v-pagination__navigation catalog-paginate__item')
 
 # сохраняем полученные данные в JSON-файл
 with open('products.json', 'w') as f:
     json.dump(result, f)
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-
-export default function App() {
-
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('path/to/products.json')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-  }, []);
-
-  const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.name}</Text>
-      <Text style={styles.subtitle}>{item.price}</Text>
-    </View>
-  );
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={item => item.name}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 24,
-  },
-  subtitle: {
-    fontSize: 18,
-    paddingTop: 10,
-  },
-});
