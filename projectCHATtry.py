f67a50e6865d4ec4810b3811ead57a188e4207f3
LaSkao <106691937+LaSkao@users.noreply.github.com>
2023-03-28 14:16:23 +0300

    Update projectCHATtry.py

--- projectCHATtry.py
+++ projectCHATtry.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 # url страницы, которую мы хотим спарсить
 url = "https://magnit.katalog-ceny.ru/index.php?route=product/category&path=118_146"
@@ -10,16 +11,31 @@ response = requests.get(url)
 # парсим HTML-код с помощью Beautiful Soup
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# находим все div-контейнеры с информацией о продуктах на первой странице
-products = soup.find_all('div', class_='product-layout product-grid col-lg-4 col-md-4 col-sm-6 col-xs-12')
+# создаем пустой список, в который будем добавлять словари для каждого продукта
+products = []
 
-# для каждого продукта получаем название, цену и скидку (если есть)
-for product in products:
+# находим все div-контейнеры с информацией о продуктах на первой странице
+
+for product in soup.find_all('div', class_='product-layout product-grid col-lg-4 col-md-4 col-sm-6 col-xs-12'):
     name = product.find('div', class_='product-name').text.strip()
     price = product.find('p', class_='price').text.strip()
-    discount = product.find('span', class_='price-old')
-    if discount:
-        discount = discount.text.strip()
-        print("Название: {}\nЦена: {}\nСкидка: {}\n".format(name, price, discount))
+
+    # проверяем наличие старой цены
+    old_price_element = product.find('span', class_='price-old')
+    if old_price_element is not None:
+        old_price = old_price_element.text.strip()
     else:
-        print("Название: {}\nЦена: {}\n".format(name, price))
+        old_price = ""
+
+    # добавляем словарь с информацией о продукте в список
+    products.append({
+        "name": name,
+        "price": price,
+        "old_price": old_price
+    })
+
+# сохраняем список продуктов в файле json
+with open("products.json", "w") as file:
+    json.dump(products, file)
+
+print("Данные сохранены в файле products.json")
